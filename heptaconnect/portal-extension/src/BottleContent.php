fcd0cc9c7853da08bd4e4460d6024f92ddfb8be7
Joshua Behrens <behrens@heptacom.de>
2020-07-24 09:28:04 +0200

    Add extension to add bottle content emitter
    SPBCC-11

--- heptaconnect/portal-extension/src/BottleContent.php
+++ heptaconnect/portal-extension/src/BottleContent.php
@@ -2,9 +2,22 @@
 
 namespace Heptacom\HeptaConnect\Playground\PortalExtension;
 
+use Heptacom\HeptaConnect\Playground\Portal\BottlePortal;
 use Heptacom\HeptaConnect\Portal\Base\Contract\PortalNodeExtensionInterface;
+use Heptacom\HeptaConnect\Portal\Base\EmitterCollection;
 use Heptacom\HeptaConnect\Portal\Base\Support\AbstractPortalNodeExtension;
 
 class BottleContent extends AbstractPortalNodeExtension implements PortalNodeExtensionInterface
 {
+    public function getEmitterDecorators(): EmitterCollection
+    {
+        return new EmitterCollection([
+            new HalfFullHalfEmptyBottleEmitter(),
+        ]);
+    }
+
+    public function supports(): string
+    {
+        return BottlePortal::class;
+    }
 }
---
+++ heptaconnect/portal-extension/src/HalfFullHalfEmptyBottleEmitter.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Playground\PortalExtension;
+
+use Heptacom\HeptaConnect\Playground\Dataset\Bottle;
+use Heptacom\HeptaConnect\Playground\Dataset\Volume;
+use Heptacom\HeptaConnect\Playground\Portal\BottlePortal;
+use Heptacom\HeptaConnect\Portal\Base\Contract\EmitContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\EmitterInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\EmitterStackInterface;
+use Heptacom\HeptaConnect\Portal\Base\MappedDatasetEntityStruct;
+use Heptacom\HeptaConnect\Portal\Base\MappingCollection;
+
+class HalfFullHalfEmptyBottleEmitter implements EmitterInterface
+{
+    public function emit(
+        MappingCollection $mappings,
+        EmitContextInterface $context,
+        EmitterStackInterface $stack
+    ): iterable {
+        /** @var MappedDatasetEntityStruct $mappedEntity */
+        foreach ($stack->next($mappings, $context) as $key => $mappedEntity) {
+            $portalNode = $context->getPortalNode($mappedEntity->getMapping());
+
+            if (!$portalNode instanceof BottlePortal) {
+                yield $key => $mappedEntity;
+                continue;
+            }
+
+            $bottle = $mappedEntity->getDatasetEntity();
+
+            if (!$bottle instanceof Bottle) {
+                yield $key => $mappedEntity;
+                continue;
+            }
+
+            $content = new Dataset\BottleContent();
+            $content->setContent(
+                (new Volume())
+                    ->setUnit(Volume::UNIT_LITER)
+                    ->setAmount($bottle->getCapacity()->getAmount() * 0.5)
+            );
+            $bottle->attach($content);
+
+            yield $key => $mappedEntity;
+        }
+    }
+
+    public function supports(): array
+    {
+        return [
+            Bottle::class,
+        ];
+    }
+}
