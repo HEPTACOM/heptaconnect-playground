7ae954094021d96468b1f82ab385a0971d9df6bb
Joshua Behrens <behrens@heptacom.de>
2020-07-24 09:27:34 +0200

    Add demo bottles and emitter
    SPBCC-11

---
+++ heptaconnect/portal/src/BottleEmitter.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Playground\Portal;
+
+use Heptacom\HeptaConnect\Playground\Dataset\Bottle;
+use Heptacom\HeptaConnect\Portal\Base\Contract\EmitContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\EmitterInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\EmitterStackInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\MappingInterface;
+use Heptacom\HeptaConnect\Portal\Base\MappedDatasetEntityStruct;
+use Heptacom\HeptaConnect\Portal\Base\MappingCollection;
+
+class BottleEmitter implements EmitterInterface
+{
+    public function emit(
+        MappingCollection $mappings,
+        EmitContextInterface $context,
+        EmitterStackInterface $stack
+    ): iterable {
+        /** @var MappingInterface $mapping */
+        foreach ($mappings as $mapping) {
+            $portalNode = $context->getPortalNode($mapping);
+
+            if (!$portalNode instanceof BottlePortal) {
+                continue;
+            }
+
+            $data = iterable_to_array($portalNode->getBottleStorage()->filter(fn (Bottle $b) => $b->getPrimaryKey() === $mapping->getExternalId()));
+
+            if (\count($data) === 0) {
+                continue;
+            }
+
+            yield new MappedDatasetEntityStruct($mapping, clone current($data));
+        }
+
+        yield from $stack->next($mappings, $context);
+    }
+
+    public function supports(): array
+    {
+        return [Bottle::class];
+    }
+}
--- heptaconnect/portal/src/BottlePortal.php
+++ heptaconnect/portal/src/BottlePortal.php
@@ -2,6 +2,14 @@
 
 namespace Heptacom\HeptaConnect\Playground\Portal;
 
+use Heptacom\HeptaConnect\Dataset\Base\Translatable\TranslatableString;
+use Heptacom\HeptaConnect\Playground\Dataset\Bottle;
+use Heptacom\HeptaConnect\Playground\Dataset\BottleCollection;
+use Heptacom\HeptaConnect\Playground\Dataset\BottleShape;
+use Heptacom\HeptaConnect\Playground\Dataset\Cap;
+use Heptacom\HeptaConnect\Playground\Dataset\Label;
+use Heptacom\HeptaConnect\Playground\Dataset\LabelCollection;
+use Heptacom\HeptaConnect\Playground\Dataset\Volume;
 use Heptacom\HeptaConnect\Portal\Base\Contract\PortalNodeInterface;
 use Heptacom\HeptaConnect\Portal\Base\EmitterCollection;
 use Heptacom\HeptaConnect\Portal\Base\ExplorerCollection;
@@ -9,6 +17,8 @@ use Heptacom\HeptaConnect\Portal\Base\ReceiverCollection;
 
 class BottlePortal implements PortalNodeInterface
 {
+    private ?BottleCollection $bottles = null;
+
     public function getExplorers(): ExplorerCollection
     {
         return new ExplorerCollection();
@@ -16,11 +26,54 @@ class BottlePortal implements PortalNodeInterface
 
     public function getEmitters(): EmitterCollection
     {
-        return new EmitterCollection();
+        return new EmitterCollection([
+            new BottleEmitter(),
+        ]);
     }
 
     public function getReceivers(): ReceiverCollection
     {
         return new ReceiverCollection();
+    }
+
+    public function getBottleStorage(): BottleCollection
+    {
+        if (is_null($this->bottles)) {
+            $this->bottles = new BottleCollection([
+                $this->generateBottle(Cap::TYPE_CROWN_CORK, 2, BottleShape::TYPE_ROUND, [
+                    $this->generateInternationalLabel('#b1900f', 'Pecl Juice'),
+                    $this->generateInternationalLabel('#80600a', 'Refreshing source to experience your inner beauty'),
+                ]),
+                $this->generateBottle(Cap::TYPE_FLIP_TOP, 1, BottleShape::TYPE_ANGULAR, [
+                    $this->generateInternationalLabel('#00ff00', 'Calming composition'),
+                    $this->generateInternationalLabel('#ff0000', 'Contains some supplements'),
+                ]),
+                $this->generateBottle(Cap::TYPE_SCREW, 0.05, BottleShape::TYPE_ROUND, [
+                    $this->generateInternationalLabel('#ffffff', 'REPL shot'),
+                    $this->generateInternationalLabel('#000000', 'POWER POWER POWER'),
+                ]),
+            ]);
+        }
+
+        return $this->bottles;
+    }
+
+    private function generateInternationalLabel(string $color, string $text): Label
+    {
+        $translatable = new TranslatableString();
+        $translatable['en'] = $text;
+
+        return (new Label())
+            ->setColor($color)
+            ->setText($translatable);
+    }
+
+    private function generateBottle(string $cap, float $volumeAmount, string $bottleShape, array $labels): Bottle
+    {
+        return (new Bottle())
+            ->setCap((new Cap())->setType($cap))
+            ->setCapacity((new Volume())->setAmount($volumeAmount, Volume::UNIT_LITER))
+            ->setShape((new BottleShape())->setType($bottleShape))
+            ->setLabels(new LabelCollection($labels));
     }
 }
