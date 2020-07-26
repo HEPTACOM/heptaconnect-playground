f8f69b32c54d50d32d7f7dba14f01d478b3a442d
Joshua Behrens <behrens@heptacom.de>
2020-07-26 21:11:34 +0200

    Move PortalNodeExtensionInterface into Portal namespace SPBCC-11

--- heptaconnect/portal-extension/src/BottleContent.php
+++ heptaconnect/portal-extension/src/BottleContent.php
@@ -3,7 +3,7 @@
 namespace Heptacom\HeptaConnect\Playground\PortalExtension;
 
 use Heptacom\HeptaConnect\Playground\Portal\BottlePortal;
-use Heptacom\HeptaConnect\Portal\Base\Contract\PortalNodeExtensionInterface;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalNodeExtensionInterface;
 use Heptacom\HeptaConnect\Portal\Base\Emission\EmitterCollection;
 use Heptacom\HeptaConnect\Portal\Base\Support\AbstractPortalNodeExtension;
 
