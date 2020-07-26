73f513a78bb121f139b51580414d2f7408e97fac
Joshua Behrens <behrens@heptacom.de>
2020-07-26 21:12:22 +0200

    Move collections into sub namespace
    SPBCC-11

--- heptaconnect/portal/src/BottlePortal.php
+++ heptaconnect/portal/src/BottlePortal.php
@@ -12,8 +12,8 @@ use Heptacom\HeptaConnect\Playground\Dataset\LabelCollection;
 use Heptacom\HeptaConnect\Playground\Dataset\Volume;
 use Heptacom\HeptaConnect\Portal\Base\Contract\PortalNodeInterface;
 use Heptacom\HeptaConnect\Portal\Base\Emission\EmitterCollection;
-use Heptacom\HeptaConnect\Portal\Base\ExplorerCollection;
-use Heptacom\HeptaConnect\Portal\Base\ReceiverCollection;
+use Heptacom\HeptaConnect\Portal\Base\Exploration\ExplorerCollection;
+use Heptacom\HeptaConnect\Portal\Base\Reception\ReceiverCollection;
 
 class BottlePortal implements PortalNodeInterface
 {
