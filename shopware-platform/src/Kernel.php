e043e925be17a20cb81d08eff5e35409e2956f60
Julian Krzefski <krzefski@heptacom.de>
2020-07-26 16:49:24 +0200

    SPBCC-11: Replace anonymous kernel with existing kernel class

--- shopware-platform/src/Kernel.php
+++ shopware-platform/src/Kernel.php
@@ -4,42 +4,14 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Playground\ShopwarePlatform;
 
-use Shopware\Core\Framework\Plugin\KernelPluginLoader\KernelPluginLoader;
-
 class Kernel extends \Shopware\Core\Kernel
 {
-    public const PLACEHOLDER_DATABASE_URL = 'mysql://_placeholder.test';
-
-    public function __construct(
-        string $environment,
-        bool $debug,
-        KernelPluginLoader $pluginLoader,
-        ?string $cacheId = null,
-        ?string $version = self::SHOPWARE_FALLBACK_VERSION
-    ) {
-        $cacheId = $cacheId ?? $environment;
-        parent::__construct($environment, $debug, $pluginLoader, $cacheId, $version);
-    }
-
     protected function initializeDatabaseConnectionVariables(): void
     {
-        $url = $_ENV['DATABASE_URL']
-            ?? $_SERVER['DATABASE_URL']
-            ?? getenv('DATABASE_URL');
-
-        if (isset($_SERVER['INSTALL']) || $url === self::PLACEHOLDER_DATABASE_URL) {
+        if ($_SERVER['INSTALL'] ?? false) {
             return;
         }
 
-        if ($this->getEnvironment() === 'dev') {
-            self::getConnection()->getConfiguration()->setSQLLogger(
-                new \Shopware\Core\Profiling\Doctrine\DebugStack()
-            );
-        }
-
-        $reflection = new \ReflectionMethod(\Shopware\Core\Kernel::class, 'initializeDatabaseConnectionVariables');
-        if (!$reflection->isPrivate()) {
-            call_user_func('parent::initializeDatabaseConnectionVariables');
-        }
+        parent::initializeDatabaseConnectionVariables();
     }
 }
