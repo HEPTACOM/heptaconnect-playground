d6e3fc918f989938c6f5f2ea706a124ce8aca305
Joshua Behrens <behrens@heptacom.de>
2020-07-26 20:35:37 +0200

    Move MappingCollection into Mapping namespace SPBCC-11

--- heptaconnect/portal-extension/src/HalfFullHalfEmptyBottleEmitter.php
+++ heptaconnect/portal-extension/src/HalfFullHalfEmptyBottleEmitter.php
@@ -9,7 +9,7 @@ use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterInterface;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterStackInterface;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitContextInterface;
 use Heptacom\HeptaConnect\Portal\Base\Mapping\MappedDatasetEntityStruct;
-use Heptacom\HeptaConnect\Portal\Base\MappingCollection;
+use Heptacom\HeptaConnect\Portal\Base\Mapping\MappingCollection;
 
 class HalfFullHalfEmptyBottleEmitter implements EmitterInterface
 {
