dcfc4b23b79671275f25b28555c05827ea344e67
Sebastian Eltermann <se@antavent.com>
2021-08-03 18:33:20 +0200

    Check if composer.lock exists in entrypoint.sh, if not run 'make shopware-platform-files' to prepare playground

--- dev-ops/docker/entrypoint.sh
+++ dev-ops/docker/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [ ! -f ContainerCreated ]; then
+    if [ ! -f ./shopware-platform/composer.lock ]; then
+        runuser -u www-data -- make --directory=/var/www/html shopware-platform-files
+    fi
     while ! mysqladmin ping -h $DATABASE_HOST --silent; do
         sleep 1
     done
-#    shopt -s globstar
     runuser -u www-data -- make --directory=/var/www/html shopware-platform-db
     touch ContainerCreated
 fi
